252460280b5ccc89afc55581b8d80b9e59b98eba
JayDee91manasa <jdmanasa.codingal@gmail.com>
2025-07-22 15:48:10 +0530

    Add files via upload

---
+++ congratulation.py
@@ -0,0 +1,7 @@
+def congratulations_message(name, achievement):
+    print(f"Congratulations, {name}!")
+    print(f"You have achieved {achievement}!")
+    print("Wishing you more success and happiness in the future!")
+name = input("Enter the person's name: ")
+achievement = input("Enter the achievement: ")
+congratulations_message(name, achievement)
