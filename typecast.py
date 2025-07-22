ebb86f93992569a0b7e9551b8cd00b8d8960ba67
JayDee91manasa <jdmanasa.codingal@gmail.com>
2025-07-22 11:35:29 +0530

    Add files via upload

---
+++ typecast.py
@@ -0,0 +1,19 @@
+number1 = 20
+number2 = 20.5
+name = "suvan"
+result = True
+con_number2= int(number2)
+print(con_number2)
+con_number1 = float(number1)
+print(con_number1)
+print("type of number 1 is ", type(number1))
+print("type of number 2 is ", type(number2))
+print("type of name is ", type(name))
+
+fname = "suvas"
+lname ="Dandu"
+print("Full name is", fname+lname)
+print(fname[::3])
+print(fname[0::1])
+print(fname[::-1])
+print("first name is:",fname.upper())
