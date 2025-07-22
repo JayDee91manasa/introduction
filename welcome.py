64915b10eafee3dbad814ba1f5efa37a333699d3
JayDee91manasa <jdmanasa.codingal@gmail.com>
2025-07-22 11:33:05 +0530

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,13 @@
+import keyword
+print(keyword.kwlist)
+print("Hello world")
+print(12340) 
+name_1= "suvas" 
+print("hello my name is", name_1, "welcome to python programming")   
+grade = 6
+print(grade)
+print("hello world", "suvas", 1234, 6)
+print("\nsuvas wants\n", "to be a software engineer")
+print("suvas is a student\n")
+print("suvas is in grade", grade)
+print("end arguement", end=" bye\n ")
